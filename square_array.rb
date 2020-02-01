191d1e23babc6d5bd4da33ab45df630596279017
Benito Galvez <mystwalker131@gmail.com>
2020-02-01 11:27:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)    
+ new_array = []
+ 
+numbers = [1,2,3] 
+ counter = 1 
+ numbers.each do|number|
+ puts number
+
+counter +=1
+
+number
+
 end
+ end
